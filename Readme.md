a451beee1c6d617aa02589e82ec33d137a6e1d58
Binary Smile <binary.smile@gmail.com>
2022-10-15 16:31:18 +0200

    Some bug fix

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Text2anki
+
+The simple script for opening the book or text and trasform it to anki card.
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import re
 
 ifile = '2022 - Paoletta - Gap size-dependent plasmonic enhancement in TJ.txt' # txt or epub
 startW = 0 # word for start
+ToLang = 'ru'
+
 
 from translate import Translator
 try:
@@ -12,7 +14,7 @@ try:
 except:
     print('Put your email adress to "email.txt" for extend limit')
     email = False
-translator = Translator(to_lang="ru",email=email)
+translator = Translator(to_lang=ToLang,email=email)
 
 
 def gettext(name):
@@ -73,18 +75,38 @@ def translateWord(word):
         if 'MYMEMORY WARNING' in value:
             print(value)
             sys.exit(1)
+        elif len(value) > 30:
+            print('Some problems with %s. Was translated as: %s' % (word,value))
         return value
     except:
         print('Problems with translation on "%s"' % word)
 
 
 def SaveData(res_dct,outfile):
-    with open(outfile, 'w', newline='') as csvfile:
+    with open(outfile, 'w', newline='',encoding="utf-8") as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['En', 'Ru'])
+        spamwriter.writerow(['En', ToLang])
         for k in res_dct:
+            print(k)
             spamwriter.writerow([k,res_dct[k]])
+
+def start_progress(title):
+    global progress_x
+    sys.stdout.write(title + ": [" + "-"*40 + "]" + chr(8)*41)
+    sys.stdout.flush()
+    progress_x = 0
+
+def progress(x):
+    global progress_x
+    x = int(x * 40 // 100)
+    sys.stdout.write("#" * (x - progress_x))
+    sys.stdout.flush()
+    progress_x = x
+
+def end_progress():
+    sys.stdout.write("#" * (40 - progress_x) + "]\n")
+    sys.stdout.flush()
 
 
 if __name__ == '__main__':
@@ -98,12 +120,14 @@ if __name__ == '__main__':
     else:
         CounterLimit = 5000 # Free, anonymous usage is limited to 5000 chars/day by MyMemory
     res_dct = {}
-
+    start_progress('Translating')
     for i in range(startW, len(stat)):
         counter += len(stat[i])
         if counter < CounterLimit:
+            progress(i/(len(stat)))
             res_dct[stat[i]] = translateWord(stat[i])
         else:
             print('The limit was reach for today. Start from %s\'s word' % i)
+    end_progress()
     SaveData(res_dct,'AnkiDesk.csv')
     print('Success')
